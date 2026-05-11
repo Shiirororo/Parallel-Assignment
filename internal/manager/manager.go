c02aa4a040be251ed4f1f1394cd4d21213974784
Shiirororo <chacchankhongphainhan313@gmail.com>
2026-05-11 23:45:23 +0700

    add Worker

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,19 +1,40 @@
 package manager
 
-type Bus struct {
+type IngressRouter struct {
+	cBus CounterBus
+	rBus ResponseBus
+	lBus LoggingBus
+}
+
+type CounterBus struct {
+	queue chan Event
+}
+type LoggingBus struct {
+	queue chan Event
+}
+type ResponseBus struct {
 	queue chan Event
 }
 
-func NewBus(size int) *Bus {
-	return &Bus{
-		queue: make(chan Event, size),
+func NewIngressRouter(size int) *IngressRouter {
+	return &IngressRouter{
+		cBus: CounterBus{queue: make(chan Event, size)},
+		rBus: ResponseBus{queue: make(chan Event, size)},
+		lBus: LoggingBus{queue: make(chan Event, size)},
 	}
 }
 
-func (b *Bus) Publish(e Event) {
-	b.queue <- e
+func (ig *IngressRouter) Publish(bus string, e Event) {
+	switch bus {
+	case "counter":
+		ig.cBus.queue <- e
+	case "response":
+		ig.rBus.queue <- e
+	case "logging":
+		ig.lBus.queue <- e
+	}
 }
 
-func (b *Bus) Queue() <-chan Event {
-	return b.queue
-}
+func (ig *IngressRouter) Counter() <-chan Event  { return ig.cBus.queue }
+func (ig *IngressRouter) Response() <-chan Event { return ig.rBus.queue }
+func (ig *IngressRouter) Logging() <-chan Event  { return ig.lBus.queue }
--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"net/http"
 	bootstrap "parallel/init"
+	"parallel/internal/manager"
 )
 
 func main() {
 	rd, db := bootstrap.Run()
 	_, _ = rd, db
-	http.HandleFunc("/class", func(w http.ResponseWriter, r *http.Request) {
+
+	router := manager.NewIngressRouter(256)
+	_ = router
+
+	http.HandleFunc("/class/id=?", func(w http.ResponseWriter, r *http.Request) {
 
 	})
 	http.ListenAndServe(":36789", nil)
